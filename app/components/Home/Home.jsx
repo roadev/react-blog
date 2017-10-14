4c2aa567f898572ef369215e56a15e8acd32c70a
Juan Roa <jdroa92@gmail.com>
2017-10-14 00:29:45 -0500

    Basic nav

--- app/components/Home/Home.jsx
+++ app/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Nav from '../Nav/Nav';
 
 const Home = () =>
   (
     <div>
+      <Nav />
       <h3>
         This is the home page
       </h3>
--- app/components/Nav/Nav.jsx
+++ app/components/Nav/Nav.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
-  <ul>
-    <li>Home</li>
-    <li>About</li>
-    <li>Posts</li>
-  </ul>
+  <nav>
+    <ul>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Nav;
